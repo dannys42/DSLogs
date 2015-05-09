e17e6348e80d9288095b33a40824798d17404ca5
Danny Sung <danny@dannysung.com>
2015-05-08 21:49:31 -0700

    Added example usage.  Updated macro parameters.
    Macro Parameters now use 'format' rather than generic 's'.

--- Example/DSLogs/DSViewController.m
+++ Example/DSLogs/DSViewController.m
@@ -17,13 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSLog(@"ENABLE_LOGS=%d\n", ENABLE_LOGS);
+    DLog(@"This is a debug line and should only appear if ENABLE_LOGS=1\n");
+    WLog(@"This line results in a warning regardless of ENABLE_LOGS.\n");
+    ELog(@"This line results in an error regardless of ENABLE_LOGS.\n");
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
---
+++ Example/Pods/Target Support Files/Pods-DSLogs-DSLogs/Pods-DSLogs-DSLogs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DSLogs_DSLogs : NSObject
+@end
+@implementation PodsDummy_Pods_DSLogs_DSLogs
+@end
